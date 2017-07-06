9c758d04af2c353372906309d2be0548c75544fb
Jonah <jonah@jonahwells.com>
2017-07-06 20:37:22 +0000

    Added pre-exising code before repo creation.

--- DateScraper.py
+++ DateScraper.py
@@ -1,1 +1,20 @@
+from lxml import html
+from bs4 import BeautifulSoup
+import re
+import requests
 
+page = requests.get('')
+data = page.text
+soup = BeautifulSoup(data, "lxml")
+pattern = "([A-Z])\w+ [0-9]{1,2}[/][0-9]{1,2}"
+matcher = re.compile(pattern)
+
+try:
+	for span in soup.find_all('span'):
+		
+		m = matcher.match(span.get_text())
+		if m:	
+			print span.get_text() 
+			print m.group() 
+except:
+	print("Couldn't parse")
