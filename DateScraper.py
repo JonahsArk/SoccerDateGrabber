aac9efbf1fe4fb26884998139d916f02600c23ba
Jonah <jonah@jonahwells.com>
2017-07-13 07:54:23 +0000

    Now grabs specified team from a table containing game information. Need to code to functions and read from command line

--- DateScraper.py
+++ DateScraper.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
+team = ''
+games = []
 page = requests.get('')
 data = page.text
 soup = BeautifulSoup(data, "lxml")
@@ -10,11 +12,32 @@ pattern = "([A-Z])\w+ [0-9]{1,2}[/][0-9]{1,2}"
 matcher = re.compile(pattern)
 
 try:
-	for span in soup.find_all('span'):
-		
-		m = matcher.match(span.get_text())
-		if m:	
-			print span.get_text() 
-			print m.group() 
+    tables = soup.findAll('table') 
+
+    for table in tables:
+        rows = table.findChildren('tr') 
+
+        for row in rows:
+            cell = row.find('td')
+            info = None
+
+            if cell is not None:
+                info = cell.find('span', text=re.compile(pattern))                
+
+            if info is not None:
+                isTeam = None
+                game = []
+                game.append(info.text)
+
+                for col in cell.find_next_siblings('td'):
+                    game.append(col.find('span').text)
+
+                    if isTeam is None and team in col.find('span').text:
+                        isTeam = True
+
+                if isTeam is not None:
+                    games.append(game)
+                
+    print games 
 except:
-	print("Couldn't parse")
+    print("Couldn't parse")
