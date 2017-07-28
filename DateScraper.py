40ddce627017852d8db569c1f02dbb7376128316
Jonah <jonah@jonahwells.com>
2017-07-28 07:44:27 +0000

    writes to games to output.csv, need to test if works in google calendar import. Also there is a bug with the list of games being doubled.. need to loook into.

--- DateScraper.py
+++ DateScraper.py
@@ -1,9 +1,11 @@
 from lxml import html
 from bs4 import BeautifulSoup
+from dateutil import parser
 import re
 import requests
 import sys
 import getopt
+import csv
 
 def main(argv):
     team = ''
@@ -33,6 +35,9 @@ def main(argv):
 def scrape_site(team, website):
     games = []
     pattern = "([A-Z])\w+ [0-9]{1,2}[/][0-9]{1,2}"
+    timepattern = "\d{1}:\d{2} (?:AM|PM)"
+    matcher = re.compile(pattern)
+    timematcher = re.compile(timepattern)
 
     try:
         page = requests.get(website)
@@ -50,7 +55,7 @@ def scrape_site(team, website):
                 info = None
 
                 if cell is not None:
-                    info = cell.find('span', text=re.compile(pattern))                
+                    info = cell.find('span', text=matcher)                
 
                 if info is not None:
                     isTeam = None
@@ -58,7 +63,7 @@ def scrape_site(team, website):
                     game.append(info.text)
 
                     for col in cell.find_next_siblings('td'):
-                        game.append(col.find('span').text)
+                        game.append(col.find('span').text)                        
 
                         if isTeam is None and team in col.find('span').text:
                             print 'Found matching row'
@@ -66,10 +71,39 @@ def scrape_site(team, website):
 
                     if isTeam is not None:
                         games.append(game)
-                
-        print games 
+        
+        print'Writing...'
+        write_output_file(games)
+        print 'Wrote to output.csv successfully' 
+
     except:
         print("Couldn't parse")
+
+def get_valid_string(date):
+    newdate = parser.parse(date)
+    return newdate 
+
+def write_output_file(games):
+    datepattern = "([A-Z])\w+ [0-9]{1,2}[/][0-9]{1,2}"
+    timepattern = "\d{1}:\d{2} (?:AM|PM)"
+    datematcher = re.compile(datepattern)
+    timematcher = re.compile(timepattern)
+
+    with open('output.csv', 'wb') as myfile:
+        writer = csv.writer(myfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Subject', 'Start Date', 'Start Time',
+                                'Description', 'Location'])
+
+        for item in games:
+            date = [x for x in item if datematcher.match(x)]
+            time = [x for x in item if timematcher.match(x)]
+            combined = ''.join('%s %s' % x for x in zip(date, time))
+            gametime = get_valid_string(combined)
+            teams = item[2] + ' vs. ' + item[3] 
+            writer.writerow(['Soccer', gametime.date(), item[1],
+                                teams, item[4]]) 
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
